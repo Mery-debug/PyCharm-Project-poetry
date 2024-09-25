a93ca81602dd5b3dac7889806b5e556ad8144044
Mery-debug <m.a.shap58000@gmail.com>
2024-09-25 23:08:05 +0300

    upd3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #from src.masks import get_mask_card_number, get_mask_account
 from src.widget import mask_account_card, get_date
 
-print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Счет 64686473678894779589'))
 print(get_date('2024-03-11T02:26:18.671407'))
 
 
