e7548194e2a47d8f65d3019be340da53e16e167f
Mery-debug <m.a.shap58000@gmail.com>
2024-09-26 23:04:33 +0300

    upd

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,8 @@ def filter_by_state(list_data: [list], state="EXECUTED") -> list:
     return list_new_data
 
 
+def sort_by_date(list_date: [list], sort=True) -> list:
+    """Function for sorting date"""
+    for lis in list_date:
+        list_date.sort(key=lambda x: x["date"], reverse=sort)
+    return list_date
