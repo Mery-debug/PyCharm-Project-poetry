26145333f66071aa16c4509db7136d5e0e658e31
Mery-debug <m.a.shap58000@gmail.com>
2024-09-27 00:15:25 +0300

    upd

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 
-def filter_by_state(list_data: [list], state="EXECUTED") -> list:
+def filter_by_state(list_data: [list[dict]], state="EXECUTED") -> list:
     """Function for search dictionary with some 'state'"""
     list_new_data = []
     for lis in list_data:
@@ -10,7 +10,7 @@ def filter_by_state(list_data: [list], state="EXECUTED") -> list:
     return list_new_data
 
 
-def sort_by_date(list_date: [list], sort=True) -> list:
+def sort_by_date(list_date: [list[dict]], sort=True) -> list:
     """Function for sorting date"""
     for lis in list_date:
         list_date.sort(key=lambda x: x["date"], reverse=sort)
