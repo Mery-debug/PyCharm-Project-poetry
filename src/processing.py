a44f9c72eb2b5de561d5c39c90c4742ec373b623
Mery-debug <m.a.shap58000@gmail.com>
2024-09-26 23:04:21 +0300

    upd

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,12 @@
 from typing import Union
 
 
-def filter_by_state(list_data: Union[list], state='EXECUTED') -> list:
+def filter_by_state(list_data: [list], state="EXECUTED") -> list:
     """Function for search dictionary with some 'state'"""
     list_new_data = []
     for lis in list_data:
-        if lis['state'] == state:
+        if lis["state"] == state:
             list_new_data.append(lis)
     return list_new_data
 
 
-def sort_by_date(list_date: [list], sort=True) -> list:
-    """Function for sorting date"""
-    for lis in list_date:
-        list_date.sort(key=lambda x: x['date'], reverse=sort)
-    return list_date
