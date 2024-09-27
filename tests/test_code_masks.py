bf31a09e51febee87f2afd3761ba797f9f4aa83a
Mery-debug <m.a.shap58000@gmail.com>
2024-09-27 18:22:42 +0300

    upd test1

--- tests/test_code_masks.py
+++ tests/test_code_masks.py
@@ -2,8 +2,13 @@ from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
 
-def test_get_mask_card_number():
+def test_get_mask_card_number_1():
     assert get_mask_card_number('0000000000000000') == '0000 00** **** 0000'
+
+
+def test_get_mask_card_number_2():
     assert get_mask_card_number('') == 'Вы ничего не ввели'
+
     assert get_mask_card_number('000000000000') == 'Ошибка ввода, мало символов'
+
     assert get_mask_card_number('12345678912345678900') == 'Ошибка ввода, много символов'
