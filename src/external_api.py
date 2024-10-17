adff4fa99b826d8114d356f4f2cd9e11fa81f23f
Mery-debug <m.a.shap58000@gmail.com>
2024-10-17 23:55:29 +0300

    env.simple2

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
 
 
 def return_cash(transactions: dict) -> float:
     if transactions['code'] == 'USD' or transactions['code'] == 'EUR':
+        load_dotenv()
+        api_key = os.getenv('API_KEY')
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={'USD'}&amount={transactions['amount']}"
 
-    return amount
+    return transactions['amount']
