76d7ffd24099e9bdadf22c193e468f139116fbe2
Mery-debug <m.a.shap58000@gmail.com>
2024-10-18 03:46:46 +0300

    external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,12 +1,32 @@
 import requests
 import os
 from dotenv import load_dotenv
+from typing import Union
 
 
-def return_cash(transactions: dict) -> float:
-    if transactions['code'] == 'USD' or transactions['code'] == 'EUR':
-        load_dotenv()
-        api_key = os.getenv('API_KEY')
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={'USD'}&amount={transactions['amount']}"
+load_dotenv('.env')
+api_key = os.getenv('API_KEY')
 
+
+def return_cash(transactions: dict) -> Union[float, str]:
+    url_1 = f"https://api.apilayer.com/exchangerates_data/latest?symbols=Rub&base={transactions['code']}"
+    response = requests.get(url_1, headers=api_key)
+    if transactions['code'] == 'USD':
+        status_code = response.status_code
+        if status_code == 200:
+            try:
+                respo = response.json()
+                return respo.loads()["rates"]["RUB"]
+            except Exception:
+                return f"Ошибка номер {status_code}"
+    elif transactions['code'] == 'EUR':
+        status_code = response.status_code
+        if status_code == 200:
+            try:
+                respo = response.json()
+                return respo.loads()["rates"]["RUB"]
+            except Exception:
+                return f"Ошибка номер {status_code}"
     return transactions['amount']
+
+
