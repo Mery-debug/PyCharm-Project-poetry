6575d55dd8dfd97f848746da57b257846428fa1b
Mery-debug <m.a.shap58000@gmail.com>
2024-10-19 04:00:18 +0300

    external_api

--- src/external_api.py
+++ src/external_api.py
@@ -9,25 +9,29 @@ api_key = os.getenv('API_KEY')
 
 
 def return_cash(transactions: dict) -> Union[float, str]:
-    url_1 = f"https://api.apilayer.com/exchangerates_data/latest?symbols=Rub&base={transactions['code']}"
+    transaction = transactions['operationAmount']['currency']['code']
+    url_1 = f"https://api.apilayer.com/exchangerates_data/latest?symbols=Rub&base={transaction}"
     response = requests.get(url_1, headers=api_key)
-    if transactions['code'] == 'USD':
+    if transaction == 'USD':
         status_code = response.status_code
         if status_code == 200:
             try:
                 respo = response.json()
-                return respo.loads()["rates"]["RUB"] * transactions['code']
+                return respo.loads()["rates"]["RUB"] * transaction
             except Exception:
                 return f"Ошибка номер {status_code}"
-    elif transactions['code'] == 'EUR':
+    elif transaction == 'EUR':
         status_code = response.status_code
         if status_code == 200:
             try:
                 respo = response.json()
-                return respo.loads()["rates"]["RUB"] * transactions['code']
+                return respo.loads()["rates"]["RUB"] * transaction
             except Exception:
                 return f"Ошибка номер {status_code}"
-    return transactions['amount']
+    else:
+        return transactions['operationAmount']['amount']
 
 
-print(return_cash())
+print(return_cash({'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount':
+    {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'EUR'}},
+    'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}))
