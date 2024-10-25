15d729d2e582d1e451d31df4433a94572ae9bff3
Mery-debug <m.a.shap58000@gmail.com>
2024-10-25 05:48:53 +0300

    new func xlsx2

--- src/financialcsvxlsx.py
+++ src/financialcsvxlsx.py
@@ -1,8 +1,9 @@
 import csv
-import pandas as df
+import pandas as pd
 
 
 def csv_reader(file_name: [str] = "../data/transactions.csv") -> list[dict]:
+    """function, which read csv files with lib csv"""
     transactions = []
     with open(file_name, encoding='utf-8') as csv_file:
         reader_dicts = csv.DictReader(csv_file, delimiter=';')
@@ -12,13 +13,51 @@ def csv_reader(file_name: [str] = "../data/transactions.csv") -> list[dict]:
 
 
 def xlsx_reader(file_name: [str] = "../data/transactions_excel.xlsx") -> list[dict]:
+    """function which read xlsx files with lib pandas"""
     transactions = []
-    pd = df.read_excel(file_name)
-    return pd
+    transaction = pd.read_excel(file_name)
+    some = pd.columns.todict()
+    for index, row in transaction.iterrows():
+        dct = some[i]: row[some[i]] for i in range()
+        transactions.append({
+
+
+
+
+print(xlsx_reader("../data/transactions_excel.xlsx"))
+# transactions_excel.xlsx
 
 
 
 
 
+
+def reader_file_transaction_excel(excel_path):
+    """Функция принимает путь до excel-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        transaction_df = pd.read_excel(excel_path)
+        transaction_list = []
+        for index, row in transaction_df.iterrows():
+            transaction_list.append(
+                {
+                    "id": str(row["id"]),
+                    "state": row["state"],
+                    "date": row["date"],
+                    "operationAmount": {
+                        "amount": str(row["amount"]),
+                        "currency": {
+                            "name": row["currency_name"],
+                            "code": row["currency_code"],
+                        },
+                    },
+                    "description": row["description"],
+                    "from": row["from"],
+                    "to": row["to"],
+                }
+            )
+    except Exception as e:
+        print(f"Error reading Excel: {e}")
+        return []
+    return transaction_list
 
 
