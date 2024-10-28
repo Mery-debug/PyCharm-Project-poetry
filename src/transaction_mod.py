8848bd038c279da65db1d055cb9262139a9bf235
Mery-debug <m.a.shap58000@gmail.com>
2024-10-28 23:53:29 +0300

    func transaction_search

--- src/transaction_mod.py
+++ src/transaction_mod.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def transaction_search(transactions: list[dict], search_str: str) -> list[dict]:
@@ -6,94 +7,14 @@ def transaction_search(transactions: list[dict], search_str: str) -> list[dict]:
     for transaction in transactions:
         for transact in transaction:
             re.findall(search_str, transact)
-        final_lst.append(transaction)
+            final_lst.append(transaction)
         return final_lst
 
 
 def find_description(transactions: list[dict], descriptions: [list]) -> dict:
-    dict_total = {}
-    i = 0
+    lst = []
     for transaction in transactions:
-        lst = re.findall(descriptions[i].lower(), transaction["description"])
-        n = len(lst)
-        dict_total[descriptions[i]] = n
-        if i > len(descriptions):
-            break
-        else:
-            i += 1
-
-    return dict_total
-
-
-result = find_description([{
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        },
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560",
-        },
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 587085106,
-            "state": "EXECUTED",
-            "date": "2018-03-23T10:45:06.972075",
-            "operationAmount": {"amount": "48223.05", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Открытие вклада",
-            "to": "Счет 41421565395219882431",
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160",
-        },
-        {
-            "id": 214024827,
-            "state": "EXECUTED",
-            "date": "2018-12-20T16:43:26.929246",
-            "operationAmount": {"amount": "70946.18", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 10848359769870775355",
-            "to": "Счет 21969751544412966366",
-        },
-        {
-            "id": 522357576,
-            "state": "EXECUTED",
-            "date": "2019-07-12T20:41:47.882230",
-            "operationAmount": {"amount": "51463.70", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 48894435694657014368",
-            "to": "Счет 38976430693692818358",
-        }], ["Перевод организации", "Перевод со счета на счет", "Открытие вклада"])
-print(result)
+        for description in descriptions:
+            if description == transaction["description"]:
+                lst.append(transaction["description"])
+    return Counter(lst)
