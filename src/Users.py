f4e1367350859eb6192ebf4053c41d7e97f8774d
Mery-debug <m.a.shap58000@gmail.com>
2024-10-31 15:23:26 +0300

    fix1

--- src/Users.py
+++ src/Users.py
@@ -16,7 +16,7 @@ def number() -> list[dict]:
                 Выберите необходимый пункт меню:
                 1. Получить информацию о транзакциях из JSON-файла
                 2. Получить информацию о транзакциях из CSV-файла
-                3. Получить информацию о транзакциях из XLSX-файла\n''')
+                3. Получить информацию о транзакциях из XLSX-файла\n''').lower()
         if user == "1":
             print("Для обработки выбран JSON-файл.")
             modul = load_transactions(os.path.join(os.path.abspath(__file__), "../../data/operations.json"))
@@ -39,7 +39,7 @@ def status(modul: list[dict]) -> list[dict]:
     """Функция выбора статуса"""
     a = 1
     while a > 0:
-        user_2 = input("Выбери статус: EXECUTED, CANCELED, PENDING\n")
+        user_2 = input("Выбери статус: EXECUTED, CANCELED, PENDING\n").upper()
         if user_2.upper() == "EXECUTED":
             print("Операции отфильтрованы по статусу: EXECUTED")
             a -= 1
@@ -64,66 +64,66 @@ def status(modul: list[dict]) -> list[dict]:
 def ad_questions(modul_transaction: list[dict]) -> list[dict]:
     """Функция, задающая дополнительные вопросы"""
     final = [{}]
-    user_3 = input("Отсортировать операции по дате? Да/Нет")
-    user_4 = input("Отсортировать по возрастанию или по убыванию?")
-    user_5 = input("Выводить только рублевые транзакции? Да/Нет")
-    user_6 = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-    if user_3.lower() == "да":
-        if user_4.lower() == "по возрастанию":
+    user_3 = input("Отсортировать операции по дате? Да/Нет").lower()
+    user_4 = input("Отсортировать по возрастанию или по убыванию?").lower()
+    user_5 = input("Выводить только рублевые транзакции? Да/Нет").lower()
+    user_6 = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет").lower()
+    if user_3 == "да":
+        if user_4 == "по возрастанию":
             sort_to_date = []
             sort = sort_by_date(modul_transaction, sorte=False)
             print(sort)
             for sor in sort:
                 sort_to_date.append(sor)
-                if user_5.lower() == "да":
+                if user_5 == "да":
                     sort_to_rub = []
                     for sort in sort_to_date:
                         if sort["operationAmount"]["currency"]["code"] == "RUB":
                             sort_to_rub.append(sort)
-                    if user_6.lower() == "да":
+                    if user_6 == "да":
                         final = sort_to_rub
-                    elif user_6.lower() == "нет":
+                    elif user_6 == "нет":
                         final = sort_to_rub
-                elif user_5.lower() == "нет":
+                elif user_5 == "нет":
                     sort_to_rub = sort_to_date
-                    if user_6.lower() == "да":
+                    if user_6 == "да":
                         final = sort_to_rub
-                    elif user_6.lower() == "нет":
+                    elif user_6 == "нет":
                         final = sort_to_rub
-        elif user_4.lower() == "по убыванию":
+        elif user_4 == "по убыванию":
             sort_to_date = []
             sort = sort_by_date(modul_transaction, sorte=True)
             for sor in sort:
                 sort_to_date.append(sor)
-                if user_5.lower() == "да":
+                if user_5 == "да":
                     sort_to_rub = []
                     for sort in sort_to_date:
                         if sort["operationAmount"]["currency"]["code"] == "RUB":
                             sort_to_rub.append(sort)
-                            if user_6.lower() == "да":
+                            if user_6 == "да":
                                 final = sort_to_rub
-                            elif user_6.lower() == "нет":
+                            elif user_6 == "нет":
                                 final = sort_to_rub
-                elif user_5.lower() == "нет":
+                elif user_5 == "нет":
                     sort_to_rub = sort_to_date
-                    if user_6.lower() == "да":
+                    if user_6 == "да":
                         final = sort_to_rub
-                    elif user_6.lower() == "нет":
+                    elif user_6 == "нет":
                         final = sort_to_rub
-    elif user_3.lower() == "нет":
-        if user_5.lower() == "да":
+    elif user_3 == "нет":
+        if user_5 == "да":
             sort_to_rub = modul_transaction
             for sor in sort_to_rub:
                 if sor["operationAmount"]["currency"]["code"] == "RUB":
                     sort_to_rub.append(sor)
-                    if user_6.lower() == "да":
+                    if user_6 == "да":
                         final = sort_to_rub
-                    elif user_6.lower() == "нет":
+                    elif user_6 == "нет":
                         final = sort_to_rub
-        elif user_5.lower() == "нет":
+        elif user_5 == "нет":
             sort_to_rub = modul_transaction
-            if user_6.lower() == "да":
+            if user_6 == "да":
                 final = sort_to_rub
-            elif user_6.lower() == "нет":
+            elif user_6 == "нет":
                 final = sort_to_rub
     return final
--- src/transaction_mod.py
+++ src/transaction_mod.py
@@ -7,12 +7,10 @@ def transaction_search(transactions: list[dict], search_str: str) -> list[dict]:
   """Функция поиска в строке по слову"""
   final_lst = []
   for transaction in transactions:
-
-
-     final_lst = re.findall(search_str, transaction.get("description"))
-
-     final_lst.append(transaction)
-  return final_lst
+    final = re.findall(search_str, transaction.get("description"))
+    if final:
+        final_lst.append(transaction)
+    return final_lst
 
 
 print(transaction_search([
